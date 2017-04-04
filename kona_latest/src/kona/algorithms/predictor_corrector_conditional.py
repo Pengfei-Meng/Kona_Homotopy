fraction to boundary rule working well. S, lam sign correct. As SVD PC is only for mu not 0, now heading to modify SVD PC for mu not 0

kona_latest/src/kona/algorithms/predictor_corrector_conditional.py
@@ -229,35 +229,53 @@ class PredictorCorrectorCnstrCond(OptimizationAlgorithm):
 
     def find_step(self, max_mu_step, x, t):
 
+            # active constraints:    s = 0 ,   lam < 0
+            # inactive constraints:   s > 0,   lam = 0   
+
             # # -- 2) new slack >= 0.0, new multipliers <= 0.0  
             # --------- fraction to boundary rule ----------
+            thresh_0 = 1e-3
 
             slack_steps = -0.995*x.primal.slack.base.data/t.primal.slack.base.data
-            if any(slack_steps > 0):
-                max_slack_step = min(slack_steps[slack_steps > 0])
+            if any(slack_steps > thresh_0):
+
+                max_slack_step = min(slack_steps[slack_steps > thresh_0])
             else:
                 max_slack_step = max_mu_step
-            max_slack_step = min(max_mu_step, max_slack_step)
+
+            # max_slack_step = min(max_mu_step, max_slack_step)
+
+            # --------- if a certain element of slack_steps (0, 1e-6) 
+            # --------- meaning this slack is hitting 0, active constraints. 
+            ind_active_s0 = np.where( (slack_steps>0) & (slack_steps<=thresh_0) )
+
 
             ineq_steps = -0.995*x.dual.base.data/t.dual.base.data
-            if any(ineq_steps > 0):
-                max_ineq_step = min(ineq_steps[ineq_steps > 0])
+
+            if any(ineq_steps > thresh_0):
+                max_ineq_step = min(ineq_steps[ineq_steps > thresh_0])
             else:
                 max_ineq_step = max_mu_step
-            max_ineq_step = min(max_mu_step, max_ineq_step)
+
+            # max_ineq_step = min(max_mu_step, max_ineq_step)
+
+            ind_inactive_lam0 = np.where( (ineq_steps>0) & (ineq_steps<=thresh_0) )
+
+            # --------- if a certain element of ineq_steps (0, 1e-6) 
+            # --------- meaning this lam is hitting 0, inactive constraints. 
 
             print 'max_slack_step, ', max_slack_step
             print 'max_ineq_step, ', max_ineq_step
 
-            if max_slack_step < 1e-6:
-                print x.primal.slack.base.data
-                pdb.set_trace()
+            # if max_slack_step < 1e-6:
+            #     print x.primal.slack.base.data
+            #     pdb.set_trace()
 
-            if max_ineq_step < 1e-6:
-                print x.dual.base.data
-                pdb.set_trace()
+            # if max_ineq_step < 1e-6:
+            #     print x.dual.base.data
+            #     pdb.set_trace()
 
-            return min(max_mu_step, max_slack_step, max_ineq_step)
+            return min(max_mu_step, max_slack_step, max_ineq_step), ind_active_s0, ind_inactive_lam0
 
 
     def solve(self):
@@ -442,7 +460,10 @@ class PredictorCorrectorCnstrCond(OptimizationAlgorithm):
             max_mu_step = (tent_mu - self.mu)/dmu
 
             if self.use_frac_to_bound is True:
-                self.step = self.find_step(max_mu_step, x, t)
+                self.step, ind_active_s0, ind_inactive_lam0 = self.find_step(max_mu_step, x, t)
+                t.primal.slack.base.data[ind_active_s0] = 0.0
+                t.dual.base.data[ind_inactive_lam0] = 0.0 
+
             else:
                 self.step = max_mu_step
 
@@ -613,7 +634,10 @@ class PredictorCorrectorCnstrCond(OptimizationAlgorithm):
 
                     # -- 2) new slack >= 0.0, new multipliers <= 0.0  
                     if self.use_frac_to_bound is True:
-                        newton_step = self.find_step(1.0, x, dx)
+                        newton_step, ind_active_s0, ind_inactive_lam0  = self.find_step(1.0, x, dx)
+                        dx.primal.slack.base.data[ind_active_s0] = 0.0
+                        dx.dual.base.data[ind_inactive_lam0] = 0.0 
+
                     else:
                         newton_step = 1.0
 

kona_latest/src/kona/linalg/matrices/preconds/svd_pc.py
@@ -215,7 +215,7 @@ class SVDPC(BaseHessian):
         #     self.W_full_inv = np.linalg.inv(self.W_full)
 
 
-    def solve(self, rhs_vec, pcd_vec):
+    def solve(self, rhs_vec, pcd_vec):    # BFGS W,   SVD on A W^{-1} A^T
         u_x = rhs_vec.primal.design.base.data
         u_s = rhs_vec.primal.slack.base.data
         u_g = rhs_vec.dual.base.data

kona_latest/src/kona/test/test_construct_svdA.py
@@ -16,7 +16,7 @@ class InequalityTestCase(unittest.TestCase):
     def setUp(self):
 
         self.outdir = './temp'
-        size_prob = 10
+        size_prob = 100
 
         self.num_design = size_prob
         self.num_ineq = size_prob
@@ -29,7 +29,7 @@ class InequalityTestCase(unittest.TestCase):
 
         # Optimizer
         optns = {
-            'max_iter' : 50,
+            'max_iter' : 200,
             'opt_tol' : 1e-7,
             'feas_tol' : 1e-7,        
             'info_file' : self.outdir+'/kona_info.dat',
@@ -44,17 +44,17 @@ class InequalityTestCase(unittest.TestCase):
                 'inner_tol' : 0.1,
                 'inner_maxiter' : 5,
                 'init_step' : 0.05,        
-                'nominal_dist' : 1.0,            
-                'nominal_angle' : 5.0*np.pi/180., 
+                'nominal_dist' : 10.0,            
+                'nominal_angle' : 20.0*np.pi/180., 
                 'max_factor' : 30.0,                  
                 'min_factor' : 0.5,                   
                 'dmu_max' : -0.0005,       
                 'dmu_min' : -0.9,      
-                'mu_correction' : 0.1,  
+                'mu_correction' : 1e-3,  
             }, 
 
             'rsnk' : {
-                'precond'       : None,     # 'svd_pc',   #'approx_adjoint', 
+                'precond'       : None,    #'svd_pc',   #'approx_adjoint', 
                 # rsnk algorithm settings
                 'dynamic_tol'   : False,
                 'nu'            : 0.95,
@@ -209,8 +209,8 @@ class InequalityTestCase(unittest.TestCase):
         self.solver.startTime = time.clock()
         file = open(self.outdir+'/kona_timings.dat', 'w')
         file.write('# Constructed_SVDA iteration timing history\n')
-        titles = '# {0:s}    {1:s}    {2:s}    {3:s}    {4:s}  \n'.format(
-            'Iter', 'Time (s)', 'Total Time (s)', 'Objective', 'max( - slack * lambda )')
+        titles = '# {0:s}    {1:s}    {2:s}    {3:s}    {4:s}   {5:s}   {6:s}\n'.format(
+            'Iter', 'Time (s)', 'Total Time (s)', 'Objective', 'max(abs(-S*Lam))', 'negative S', 'postive Lam' )
         file.write(titles)
         file.close()
 